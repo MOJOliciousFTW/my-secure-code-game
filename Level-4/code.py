6abc4b3f27692018ad0c8755342dff29acd74b68
Johannes Møgster <johannes.mogster@mojotech.no>
2023-04-06 22:23:41 +0000

    level-4, this got weird. should be allowed to fail

--- Level-4/code.py
+++ Level-4/code.py
@@ -122,16 +122,12 @@ class DB_CRUD_ops(object):
             cur = db_con.cursor()
             
             res = "[METHOD EXECUTED] get_stock_price\n"
-            query = "SELECT price FROM stocks WHERE symbol = '" + stock_symbol + "'"
+            query = "SELECT price FROM stocks WHERE symbol = '{0}'".format(stock_symbol.split("';")[0])
             res += "[QUERY] " + query + "\n"
-            if ';' in query:
-                res += "[SCRIPT EXECUTION]\n"
-                cur.executescript(query)
-            else:
-                cur.execute(query)
-                query_outcome = cur.fetchall()
-                for result in query_outcome:
-                    res += "[RESULT] " + str(result) + "\n"
+            cur.execute("SELECT price FROM stocks WHERE symbol = ?", (stock_symbol.split("';")[0], ))
+            query_outcome = cur.fetchall()
+            for result in query_outcome:
+                res += "[RESULT] " + str(result) + "\n"
             return res
                 
         except sqlite3.Error as e:
